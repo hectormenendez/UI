1ff7659296ef3e0b32c897e9b6d86ffa024c6f5b
Hector Menendez <hamr23@gmail.com>
2011-09-01 15:21:00 -0500

    Syncronic loading of elements enabled.

---
+++ js/ui.modal.js
@@ -0,0 +1,70 @@
+/**
+ * Modal Dialog.
+ * @note REMEMBER: this is scoped to "ui".
+ * @created 2011/AUG/31 13:44
+ **/
+var uiSource = {
+
+	 core:null, // core UI object.
+	$this:null, // current jQuery element.
+
+	settings:{
+		auto  : false,
+		speed : 0      // 0=instant, int=miliseconds 'slow','fast','normal'
+	},
+
+	/**
+	 * Construct modal.
+	 */
+	__construct:function($this, core){
+		// make element visible to all methods.
+		this.$this = $this;
+		this.core  = core;
+		// @NOTE I'm assuming this.$overlay exists, since is set by core.
+		// put contents inside a section
+		var html     = $this.html();
+		var $section = $this.html('').append('<section>'+html+'</section');
+		// add ui-classes to children.
+		core.children($section);
+		// obtain title. {TODO} what happens if it doesn't exist?
+		var title = $this.attr('title');
+		// save submit or reset elements and move'em to the footer.
+		var $button = $this.find('input[type="submit"],input[type="reset"]').remove();
+		// Create footer and header
+		$this.$header = $('<header></header>').prependTo($this).append('<h2>'+title+'<h2>');
+		$this.$footer = $('<footer></footer>').appendTo($this).append($button);
+		if (core.settings.modal.auto) this.show();
+		if (core.settings.debug) console.info('modal: constructed succesfully.', core.modal);
+	},
+
+	/**
+	 * @author Hector Menendez <h@cun.mx>
+	 * @created 2011/SEP/01 14:48
+	 */
+	hide:function(){
+		this.core.$overlay.hide();
+		this.$$this.hide();
+		// don't break jQuery's chain.
+		return this.$$this;
+	},
+
+	/**
+	 * @author Hector Menendez <h@cun.mx>
+	 * @created 2011/SEP/01 14:49
+	 */
+	show:function(){
+		var $o = this.core.$overlay;
+		var $e = this.$this;
+		// get opacity for overlay from css.
+		var o = $o.css('opacity');
+		$e.css('opacity',0).show().animate({opacity:1},this.core.settings.modal.speed);
+		$o.css('opacity',0).show().animate({opacity:o},this.core.settings.modal.speed);
+		// center on screen
+		$e.css({
+			marginLeft : -1*($e.width() /2),
+			marginTop  : -1*($e.height()/2)
+		});
+		return $e;
+	}
+
+};
--- ui.js
+++ ui.js
@@ -4,12 +4,62 @@
  */
 
 (function($,undefined){
-
 var ui = {};
 
 ui.settings = {
-	debug:false
+	debug:true
 };
+
+ui.isset    = null;
+ui.$body    = null;
+ui.$overlay = null;
+ui.$loader  = null;
+
+/**
+ * @author Hector Menendez <h@cun.mx>
+ * @created 2011/SEP/01 09:02
+ */
+ ui.__construct = function(){
+	// cache body element.
+	ui.$body = $('body');
+	// these cannot be set by the user.
+	$('.ui-overlay,.ui-hider').remove();
+	// overlay and hider must alway be the only direct siblings of body.
+	ui.$body.prepend('<div class="ui-overlay"></div><div class="ui-loader"></div>');
+	ui.$overlay = ui.$body.find('.ui-overlay');
+	ui.$loader  = ui.$body.find('.ui-loader');
+	// set baseurl
+	$('script').each(function(){
+		if (this.src.substr(-5)!='ui.js') return;
+		var url = this.src.substr(0, this.src.length-5);
+		//	Removed because location.origin is not defined in FF-
+		//	TODO: use location href to detect cross domain.
+		//	#	if (url.indexOf(location.origin) !== 0)
+		//	#	ui.error('Cross-domain script loading is not supported');
+		ui.settings.baseurl = url;
+	});
+	ui.isset = true;
+};
+
+/**
+ * @author Hector Menendez <h@cun.mx>
+ * @created 2011/SEP/01 08:33
+ */
+ui.loader      = {};
+ui.loader.show = function(){
+	ui.$overlay.show();
+	ui.$loader.show();
+};
+
+/**
+ * @author Hector Menendez <h@cun.mx>
+ * @created 2011/SEP/01 13:55
+ */
+ui.loader.hide = function(){
+	ui.$overlay.hide();
+	ui.$loader.hide();
+};
+
 
 /**
  * Redirect all calls to widget counterparts according to classname.
@@ -20,28 +70,57 @@ ui.settings = {
  * @working 2011/AUG/31 04:57
  * @created 2011/AUG/31 04:15
  */
-ui.init = function(settings, core){
-	// if core settings are sent, merge'em.
-	if (typeof core == 'object')
-		for (var i in core) if (core.hasOwnProperty(i)) ui.settings[i] = core[i];
-	var c, pos;
-	// obtain ui-class first.
-	c = this.attr('class');
-	if ((pos=c.indexOf('ui-')) == -1)
-		return ui.error('Provide valid ui-object');
-	c = c.substring(pos+3);
-	c = c.substring(0,c.search(/\s|$/gm));
-	// extend settings with class-specific [if available]
-	var sets = new Object;
-	sets[c] = settings;
-	if (!ui.extend(sets)) return this;
-	// update settings.
-	ui[c].settings = ui.settings[c];
-	// construct, instance and return.
-	ui[c].__construct(this);
-	var $this  = this;
-	$this[c] = ui[c];
-	delete $this[c].__construct;
+ui.init = function(settings){
+	// if ui has not been set, do so.
+	if (!ui.isset) ui.__construct();
+	// start loader
+	ui.loader.show();
+	// obtain ui-class name
+	var $this = this;
+	var cname = $this.attr('class');
+	var pos;
+	// extract valid ui-classname from class string.
+	if ((pos = cname.indexOf('ui-')) == -1) return ui.error('Invalid ui-element');
+	cname = cname.substring(pos+3);
+	cname = cname.substring(0, cname.search(/\s|$/gm));
+	// load script from remote file.
+	$.ajax({
+		type:'GET',
+		url: ui.settings.baseurl+'js/ui.'+cname+'.js',
+		cache:ui.settings.debug? false : true,  // you won't see your changes if caching's enabled.
+		async:ui.settings.debug? true  : false, // errors won't be catched in syncronic mode.
+		data: null,
+		dataType:'script',
+		beforeSend:function(x){
+			// get around firefox 3 bug.
+			if (x.overrideMimeType) x.overrideMimeType('text/plain');
+		},
+		success:function(response, status, data){
+			if (ui.settings.debug) console.info(cname+': '+status,data);
+			ui.loader.hide();
+			// imported script succesfully
+			// merge default settings with user's
+			settings = $.extend({}, uiSource.settings, settings);
+			// now  merge them with global settings-
+			var sets = {};
+			sets[cname] = settings;
+			ui.extend(sets);
+			// delete local settings, we must always use global's.
+			delete uiSource.settings;
+			// append/replace element to ui
+			ui[cname] = uiSource;
+			// construct element, with this scope.
+			ui[cname].__construct($this, ui);
+			// public local methods to the jQuery element.
+			$this[cname] = ui[cname];
+			// delete constructor from public scope-
+			delete $this[cname].__construct;
+		},
+		error:function(data){
+			ui.loader.hide();
+			ui.error('Script did not load correctly. ['+data.statusText+']');
+		}
+	});
 	return $this;
 };
 
@@ -57,7 +136,7 @@ ui.extend = function(settings){
 		else if (settings[i] === undefined) return true;
 		// extend core settings
 		for (var j in settings[i]){
-			if (ui.settings[i] === undefined) ui.settings[i] = new Object;
+			if (ui.settings[i] === undefined) ui.settings[i] = {};
 			ui.settings[i][j] = settings[i][j];
 		}
 		return ui.settings[i];
@@ -80,77 +159,23 @@ ui.children = function(parent){
 		var node = this.nodeName.toLowerCase();
 		if (allow.indexOf(' '+node+' ') == -1) return;
 		$(this).addClass('ui-'+node);
-	})
-}
-
-/**
- * Modal Dialog.
- *
- * Requirements: .ui-overlay as direct child of body.
- **/
- 
-ui.modal = {
-
-	// set defaults.
-	settings:ui.extend({modal:{
-		auto  : false,
-		speed : 0,  // 0=instant, int=miliseconds 'slow','fast','normal'
-	}}),
-
-	$overlay:null,
-	$element:null,
-
-	/**
-	 * Construct modal.
-	 */
-	__construct:function(element){
-		ui.modal.$element = element;
-		if (!ui.modal.$overlay) ui.modal.$overlay = $('.ui-overlay');
-		if (!ui.modal.$overlay.length) return ui.error('An .ui-overlay element is required.');
-		if (!ui.modal.$overlay.length>1) return ui.error('Only one .ui-overlay element is needed');
-		ui.modal.hide();
-		// begin processing.
-		// contens should be inside a section elememt
-		var html = element.html();
-		var $s = element.html('').append('<section>'+html+'</section');
-		// add ui-classes to children.
-		ui.children($s);
-		// obtain title. {TODO} what happens if it doesn't exist?
-		var t = element.attr('title');
-		// filter out subnmit or reset buttons, if existent they will replace ours.
-		var $b = element.find('input[type="submit"],input[type="reset"]').remove();
-		// Create footer and header
-		var $h = $('<header></header>').prependTo(element).append('<h2>'+t+'<h2>');
-		var $f = $('<footer></footer>').appendTo(element).append($b);
-		if (ui.settings.modal.auto) ui.modal.show();
-	},
-
-	hide:function(){
-		this.$overlay.hide();
-		this.$element.hide();
-		return this.$element
-	},
-
-	/**
-	 * Show Modal.
-	 */
-	show:function(){
-		var $o = this.$overlay;
-		var $e = this.$element;
-		// get opacity for overlay from css.
-		var o = $o.css('opacity');
-		$e.css('opacity',0).show().animate({opacity:1},this.settings.speed);
-		$o.css('opacity',0).show().animate({opacity:o},this.settings.speed);
-		// center on screen
-		$e.css({
-			marginLeft : -1*($e.width() /2),
-			marginTop  : -1*($e.height()/2)
-		});
-		return $e;
-	}
+	});
 };
 
+/**
+ * Allow user to pass global settings.
+ * @author Hector Menendez <h@cun.mx>
+ * @created 2011/SEP/01 03:48
+ */
+ui.globalsettings = function(settings){
+	if (typeof settings == 'object')
+		for (var i in settings)
+			if (settings.hasOwnProperty(i)) ui.settings[i] = settings[i];
+	else if (settings !== undefined) return  ui.error('Settings are required.');
+};
+
+$.ui    = ui.globalsettings;
 $.fn.ui = ui.init;
 
-})(jQuery)
+})(jQuery);
 
